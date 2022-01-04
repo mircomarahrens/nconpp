e6f8433a6734978ca5abe078afb4f5f24ec45a19
Mirco-Marahrens <mirco.marahrens@kyndryl.com>
2022-01-04 16:10:53 +0100

    update

--- nconpp-python/src/main.cpp
+++ nconpp-python/src/main.cpp
@@ -1,17 +1,11 @@
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
-#include "xtensor/xmath.hpp"
-#include "xtensor/xarray.hpp"
-
 #define FORCE_IMPORT_ARRAY
 #include "xtensor-python/pyarray.hpp"
 #include "xtensor-python/pyvectorize.hpp"
 
 #include <complex>
-#include <cmath>
-#include <iostream>
-#include <numeric>
 #include <optional>
 
 #include "NetworkContractor.h"
@@ -25,26 +19,12 @@ T contract_wrapper(std::vector<T>& containerList,
 	std::optional<std::vector<int>> contractionSequenceLegs,
 	std::optional<std::vector<int>> finalOrder)
 {
-	// if empty fill with defaults
 	std::vector<int> _contractionSequenceLegs = {};
-	if (!contractionSequenceLegs.has_value()) {
-		set<int> conSet = Container::allUniqueIntegersSorted(legsList);
-		_contractionSequenceLegs.assign(conSet.begin(), conSet.end());
-		Container::removeNegatives(_contractionSequenceLegs);
-	}
-	else
-	{
+	if (contractionSequenceLegs.has_value()) {
 		_contractionSequenceLegs = contractionSequenceLegs.value();
 	}
 	std::vector<int> _finalOrder = {};
-	if (!finalOrder.has_value())
-	{
-		set<int> conSet = Container::allUniqueIntegersSorted(legsList);
-		_finalOrder.assign(conSet.begin(), conSet.end());
-		Container::removePositives(_finalOrder);
-		std::reverse(_finalOrder.begin(), _finalOrder.end());
-	}
-	else
+	if (finalOrder.has_value())
 	{
 		_finalOrder = finalOrder.value();
 	}
@@ -75,7 +55,7 @@ PYBIND11_MODULE(_nconpp, m)
 	               hence occuring in pairs
 	             - legs with negative integers won't be contracted, so called
 	               dangling legs
-	     contractionSequenceTensors (optional):
+	     contractionSequenceLegs (optional):
 	         order in which the tensors shall be contracted
 	     finalOrder (optional):
 	         Permutation of the legs of the final tensor.
@@ -83,13 +63,6 @@ PYBIND11_MODULE(_nconpp, m)
 	    @return:
 	     the final contracted container
     )pbdoc";
-
-    //m.def("contract", &NetworkContractor::contract<xt::pyarray<std::complex<double>, xt::layout_type::dynamic>>);
-
-    //m.def("add", &NetworkContractor::add);
-
-    //m.def("add", &NetworkContractor::add<std::complex<double>>);
-    //m.def("add2", &NetworkContractor::add<xt::pyarray<std::complex<double>>>);
 
     m.def("contract",
         &contract_wrapper<xt::pyarray<std::complex<double>>>,
