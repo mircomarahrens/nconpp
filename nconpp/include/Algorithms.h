03985ce359e3e1526451809da1c29e35c4590928
Mirco Marahrens <mirco.marahrens@googlemail.com>
2022-03-30 21:19:02 +0200

    move implementation to .cpp

--- nconpp/include/Algorithms.h
+++ nconpp/include/Algorithms.h
@@ -4,39 +4,14 @@
 
 class Algorithms {
 public:
-    static std::vector<std::set<int>> getConnectedComponents(const std::size_t size,
-                                                             const std::vector<std::vector<int>> &adjacencyList) {
-        std::vector<std::set<int>> connectedComponents = {};
-
-        std::vector<bool> visited(size);
-        for (int v = 0; v < size; v++)
-            visited[v] = false;
-
-        std::set<int> component;
-        for (int v = 0; v < size; v++) {
-            if (!visited[v]) {
-                // depth first search
-                DFSVisit(v, visited, component, adjacencyList);
-                connectedComponents.emplace_back(component);
-                component.clear();
-            }
-        }
-
-        return connectedComponents;
-    };
+    static std::vector<std::set<int>> getConnectedComponents(std::size_t size,
+                                                             const std::vector<std::vector<int>> &adjacencyList);
 
 private:
     static void DFSVisit(int v,
                          std::vector<bool> &visited,
                          std::set<int> &component,
-                         const std::vector<std::vector<int>> &adjacencyList) {
-        visited[v] = true;
-        component.insert(v);
-
-        for (auto i = adjacencyList[v].begin(); i != adjacencyList[v].end(); ++i)
-            if (!visited[*i])
-                DFSVisit(*i, visited, component, adjacencyList);
-    };
+                         const std::vector<std::vector<int>> &adjacencyList);
 
     static void BFSVisit(int v,
                          std::vector<int> &visited,
--- nconpp/include/Container.h
+++ nconpp/include/Container.h
@@ -7,69 +7,58 @@
 #include <set>
 #include <numeric>
 
-class Container
-{
+class Container {
 public:
-    static void removeNegatives(std::vector<int>& vec)
-    {
+    static void removeNegatives(std::vector<int> &vec) {
         vec.erase(
-            remove_if(
-                vec.begin(), vec.end(), isNegative),
-            vec.end());
+                remove_if(
+                        vec.begin(), vec.end(), isNegative),
+                vec.end());
     };
 
-    static void removeNegatives(std::set<int>& set)
-    {
-        for (auto itr = set.begin(); itr != set.end();)
-        {
+    static void removeNegatives(std::set<int> &set) {
+        for (auto itr = set.begin(); itr != set.end();) {
             if (isNegative(*itr))
                 set.erase(itr++);
         }
     };
 
-    static void removePositives(std::vector<int>& vec)
-    {
+    static void removePositives(std::vector<int> &vec) {
         vec.erase(
-            remove_if(
-                vec.begin(), vec.end(), isPositive),
-            vec.end());
+                remove_if(
+                        vec.begin(), vec.end(), isPositive),
+                vec.end());
     };
 
-    static void removePositives(std::set<int>& set)
-    {
-        for (auto itr = set.begin(); itr != set.end();)
-        {
+    static void removePositives(std::set<int> &set) {
+        for (auto itr = set.begin(); itr != set.end();) {
             if (isPositive(*itr))
                 set.erase(itr++);
         }
     };
 
     static std::set<int> allUniqueIntegersSorted(
-        const std::vector<std::vector<int>>& legsIndex)
-    {
+            const std::vector<std::vector<int>> &legsIndex) {
         std::set<int> conSet;
-        for (std::vector<int> legs : legsIndex)
-        {
+        for (std::vector<int> legs: legsIndex) {
             std::sort(legs.begin(), legs.end());
             std::set_union(legs.begin(), legs.end(),
-                conSet.begin(), conSet.end(),
-                std::inserter(conSet, conSet.end()));
+                           conSet.begin(), conSet.end(),
+                           std::inserter(conSet, conSet.end()));
         }
         return conSet;
     };
 
-    static std::set<int> createRangeSet(size_t size)
-    {
+    static std::set<int> createRangeSet(size_t size) {
         std::vector<int> vec(size);
         std::iota(vec.begin(), vec.end(), 0);
         return std::set<int>(vec.begin(), vec.end());
     };
 
-    template <typename dtype>
+    template<typename dtype>
     static std::optional<int> getIndexToElement(std::vector<dtype> vec,
-        dtype element,
-        int pos = 0)
-    {
+                                                dtype element,
+                                                int pos = 0) {
         auto it = std::find(vec.begin() + pos, vec.end(), element);
         if (it != vec.end())
             return int(std::distance(vec.begin(), it));
@@ -77,63 +66,56 @@ public:
     };
 
     static std::vector<int> getIntersection(std::vector<int> vec1,
-        std::vector<int> vec2)
-    {
+                                            std::vector<int> vec2) {
         std::sort(vec1.begin(), vec1.end());
         std::sort(vec2.begin(), vec2.end());
 
         std::vector<int> intersec = {};
         std::set_intersection(vec1.begin(), vec1.end(),
-            vec2.begin(), vec2.end(),
-            std::back_inserter(intersec));
+                              vec2.begin(), vec2.end(),
+                              std::back_inserter(intersec));
 
         return intersec;
     }
 
-    template <typename T, typename S>
+    template<typename T, typename S>
     // requires std::is_convertible_v<S::value_type, std::size_t>
-    static void removeIndicesFromVector(std::vector<T>& v, const S& rm)
-    {
+    static void removeIndicesFromVector(std::vector<T> &v, const S &rm) {
         using std::begin;
         using std::end;
         return removeIndicesFromVector(v, begin(rm), end(rm));
     }
 
 private:
-    static bool isNegative(int i)
-    {
+    static bool isNegative(int i) {
         return i < 0;
     };
-    static bool isPositive(int i)
-    {
+
+    static bool isPositive(int i) {
         return i > 0;
     };
 
-    template <typename T, typename Iter>
-    static void removeIndicesFromVector(std::vector<T>& v, Iter begin, Iter end)
-        // requires std::is_convertible_v<std::iterator_traits<Iter>::value_type, std::size_t>
+    template<typename T, typename Iter>
+    static void removeIndicesFromVector(std::vector<T> &v, Iter begin, Iter end)
+    // requires std::is_convertible_v<std::iterator_traits<Iter>::value_type, std::size_t>
     {
         std::size_t current_index = 0;
 
-        if (std::is_sorted(begin, end))
-        {
+        if (std::is_sorted(begin, end)) {
 
             // sorted version - advance through begin..end
             auto rm_iter = begin;
-            const auto pred = [&](const T&) {
+            const auto pred = [&](const T &) {
                 // anymore to remove?
-                if (rm_iter != end && *rm_iter == current_index++)
-                {
+                if (rm_iter != end && *rm_iter == current_index++) {
                     return ++rm_iter, true;
                 }
                 return false;
             };
             v.erase(std::remove_if(v.begin(), v.end(), pred), v.end());
-        }
-        else
-        {
+        } else {
             // unsorted version - search for each index in begin..end
-            const auto pred = [&](const T&) {
+            const auto pred = [&](const T &) {
                 return std::find(begin, end, current_index++) != end;
             };
             v.erase(std::remove_if(v.begin(), v.end(), pred), v.end());
