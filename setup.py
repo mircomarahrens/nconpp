fc2c1372794bfbfd9fb795fcd9ace3e6d93dc962
Mirco Marahrens <mirco.marahrens@googlemail.com>
2022-04-09 13:50:14 +0200

    include python module installation

---
+++ setup.py
@@ -0,0 +1,25 @@
+from skbuild import setup
+from setuptools import find_packages
+
+# cmake specific
+__cmake_args__= []
+# For our 3rd party library handling we used the C++ package manager vcpkg
+# (see https://github.com/microsoft/vcpkg for details). Vcpkg offers a cmake
+# toolchain file which we have to reveal to the cmake installer.
+__toolchain__ = "/home/mircomarahrens/vcpkg/scripts/buildsystems/vcpkg.cmake"
+__cmake_args__.append("-DCMAKE_TOOLCHAIN_FILE={}".format(__toolchain__))
+
+setup(
+    name="nconpp",
+    version="0.1",
+    description="Library for Tensor Network contraction.",
+    author="Mirco Marahrens",
+    author_email="mirco.marahrens@googlemail.com",
+    url="https://github.com/mircomarahrens/nconpp-python",
+    packages=find_packages(where="nconpp-python", include=["numpy"]),
+    package_dir={"": "nconpp-python"},
+    cmake_install_dir="nconpp-python/nconpp",
+    cmake_args=__cmake_args__,
+    include_package_data=True,
+    python_requires=">=3.6",
+)
