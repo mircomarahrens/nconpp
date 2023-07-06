d37698482e8b3e4cbcc271fb9a229dadeee5487a
Mirco Marahrens <mirco.marahrens@googlemail.com>
2023-07-06 10:52:59 +0200

    up

--- python/nconpp/__init__.py
+++ python/nconpp/__init__.py
@@ -3,6 +3,6 @@ import os, sys
 if (sys.platform == 'win32'):
     os.add_dll_directory("C:/Repositories/nconpp/out/build/python/vcpkg_installed/x64-windows/bin/")
 
-from ._nconpp import TensorNetwork, Graph
+from ._nconpp import TensorNetwork, Graph, VertexProperties, EdgeProperties
 
-__all__ = ("TensorNetwork", "Graph")
+__all__ = ("TensorNetwork", "Graph", "VertexProperties", "EdgeProperties")
