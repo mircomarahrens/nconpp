85c050e3a174d7dab359ed1f40b2767b1a2fe8eb
Mirco Marahrens <mirco.marahrens@googlemail.com>
2023-10-21 10:28:35 +0200

    init

---
+++ build.py
@@ -0,0 +1,11 @@
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+def build(setup_kwargs):
+    ext_modules = [
+        Pybind11Extension("nconpp", ["python/src/nconpp.cpp"]),
+    ]
+    setup_kwargs.update({
+        "ext_modules": ext_modules,
+        "cmd_class": {"build_ext": build_ext},
+        "zip_safe": False,
+    })
